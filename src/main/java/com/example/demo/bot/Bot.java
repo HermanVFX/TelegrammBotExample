b16bf999674d66e089af599da5b1f5c4099ca227
Махиянов Герман Раилевич <gmahiyanov@aviasoft.ru>
2023-12-24 13:30:00 +0300

    Конфиги

--- src/main/java/com/example/demo/bot/Bot.java
+++ src/main/java/com/example/demo/bot/Bot.java
@@ -7,29 +7,66 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.logging.Level;
+
 @Component
 public class Bot extends TelegramLongPollingBot {
-    @Value("${bot.name}")
-    private String botUsername;
 
-    @Value("${bot.token}")
-    private String botToken;
+    private static final String START = "/start";
+
+    public Bot(@Value("${bot.token}") String botToken) {
+        super(botToken);
+    }
     @Override
     public String getBotUsername() {
-        return botUsername;
+        return "realBestPremiumBot";
     }
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
     @Override
     public void onUpdateReceived(Update update) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            return;
+        }
+        var message = update.getMessage().getText();
+        var chatId = update.getMessage().getChatId();
+        switch (message) {
+            case START -> {
+                String userName = update.getMessage().getChat().getUserName();
+                startCommand(chatId, userName);
+            }
+            default -> unknownCommand(chatId);
+        }
+    }
+    private void startCommand(Long chatId, String userName) {
+        var text = """
+                Привет кожанные ублюдки
+                """;
+        var formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+
+
+
+    private void helpCommand(Long chatId) {
+        var text = """
+                Справочная информация по боту
+                                TODO
+                """;
+        sendMessage(chatId, text);
+    }
+
+    private void unknownCommand(Long chatId) {
+        var text = "Не удалось распознать команду!";
+        sendMessage(chatId, text);
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
         try {
-            execute(new SendMessage().setChatId(update.getMessage().getChatId())
-                    .setText("Hi!"));
+            execute(sendMessage);
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            System.out.println("МЫ ВСЕ ПРОЕБАЛИ");
         }
     }
 }
